3cbc488a0d27fb15b4dc120ffe23e0998aac4249
Abhishek Kashyap <akkashyap479@gmail.com>
2025-06-07 11:48:18 +0530

    Updated

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 const connectDB = async () => {
   try {
 mongoose.connection.on("connected", () => {
     console.log("MongoDB connection established successfully.");
+    
   })
 
 
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`, {
- 
- 
-    });
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
